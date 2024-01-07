51784605738c5b521edac6b11a46c00b6fa65dc0
Bin <45880588+Alicebond@users.noreply.github.com>
2024-01-07 13:49:13 -0330

    Remove redundant code

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
 const compression = require("compression");
 const helmet = require("helmet");
@@ -51,7 +50,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler

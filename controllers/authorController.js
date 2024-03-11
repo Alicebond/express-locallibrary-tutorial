3e3fbab47a24faa7ca820ff11da7594fbcbb2792
Bin <45880588+Alicebond@users.noreply.github.com>
2024-03-10 21:43:06 -0230

    typo

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -50,9 +50,9 @@ exports.author_create_post = [
     .trim()
     .isLength({ min: 1 })
     .escape()
-    .withMessage("First name must be specified.")
+    .withMessage("Family name must be specified.")
     // .isAlphanumeric()
-    .withMessage("First name has non-alphanumeric characters."),
+    .withMessage("Family name has non-alphanumeric characters."),
   body("date_of_birth", "Invalid date of birth")
     .optional({ values: "falsy" })
     .isISO8601()
